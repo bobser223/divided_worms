3836c72b1511d402b9011e5f466cb29c0639ce88
Soto <bylina.alexa2005@gmail.com>
2025-07-07 18:12:09 +0300

    weapon

---
+++ weapon/bullet_prototype.cpp
@@ -0,0 +1,17 @@
+//
+// Created by bylin on 07.07.2025.
+//
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+vector<string> weapons{"rocket_launcher", "grenade", "shotgun", "uzi"}
+
+class Bullet
+private:
+    string type;
+    int damage;
+    vector<int> trajectory;
+    string shape;
+
