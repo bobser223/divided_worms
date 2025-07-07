78f06a77a6a2a04e1dc21b1c1e90b8f3cde84c9c
Soto <bylina.alexa2005@gmail.com>
2025-07-07 19:03:45 +0300

    bullet

--- weapon/bullet_prototype.cpp
+++ weapon/bullet_prototype.cpp
@@ -4,14 +4,39 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <chrono>
+#include <thread>
 using namespace std;
 
-vector<string> weapons{"rocket_launcher", "grenade", "shotgun", "uzi"}
+vector<string> weapons{"rocket_launcher", "shotgun", "uzi"};
 
-class Bullet
+
+class Bullet(vector<pair> trajectory)
 private:
     string type;
     int damage;
-    vector<int> trajectory;
     string shape;
+    int recoil;
+    int position;
+    float time;
+
+public:
+    int current_status;
+
+    void setTrajectory(vector<pair> trajectory);
+
+    void change_position(){
+        const auto delay = chrono::milliseconds(time);
+        current_status = 2;
+        for (int i = 0; i < trajectory.size(); ++i) {
+
+            this_thread::sleep_for(delay);
+            position = i;
+        }
+        current_status = 3;
+    }
+
+    void visual_change_position(){}
+
+
 
